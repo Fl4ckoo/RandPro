beb3bec86ee06ef0d0c844e62b14d363869b3c04
Fl4ckoo <rati.gogashvili.1@btu.edu.ge>
2023-07-18 03:44:06 +0400

    Update to Lucky Spin Wheel

--- js/script2.js
+++ js/script2.js
@@ -1,46 +1,69 @@
-let myArr = ['R','A','T','I','R','A','T','I'];
-myArr.length = length;
+let myArr = [' ',' ',' ',' ',' ',' ',' ',' '];
+let delArray = [' ',' ',' ',' ',' ',' ',' ',' '];
 let element;
 let text = "";
-let index = 7;
+let clickCount = -1;
 
 
 
 let wheel = document.querySelector('.wheel');
-let value = Math.ceil(Math.random() * 3600);
+let randWheel = Math.ceil(Math.random() * 3600);
 
 
 document.getElementById('btnAdd').onclick = function() {
     
     element = document.getElementById('inpName').value;
-
-   
-
     
-
-    
-
-
     if(element == "") {
         alert('Please enter a name');
     }else{
-    
-        myArr.unshift(element);
-        
+
+        clickCount++;
+
+        switch(clickCount){
+            case 0:
+               myArr[0] = element;
+               break;
+            case 1:
+               myArr[1] = element;
+               break;
+            case 2:
+               myArr[2] = element;
+               break;
+            case 3:
+               myArr[3] = element;
+               break;
+            case 4:
+               myArr[4] = element;
+               break;
+            case 5:
+               myArr[5] = element;
+               break;
+            case 6:
+               myArr[6] = element;
+               break;
+            case 7:
+               myArr[7] = element;
+               break;
+            default:
+               alert("Maxed out elements!\nSpin The wheel");    
+       }
+
         text = "";
 
         for(let i = 0; i < myArr.length; i++) {
             text = text + myArr[i] + "<br>";
         }
 
-        length = myArr.length;
 
-        document.getElementById('quentity').innerHTML = "Members of the Wheel : " + length;
+        document.getElementById('quentity').innerHTML = "Members of the Wheel : ";
 
 
         document.getElementById('labelName').innerHTML = text;
 
-        //Declare the Wheel Values;
+        
+
+        //Declare the Wheel elements;
         document.getElementById('l1').innerHTML = myArr[0];
         document.getElementById('l2').innerHTML = myArr[1];
         document.getElementById('l3').innerHTML = myArr[2];
@@ -52,10 +75,42 @@ document.getElementById('btnAdd').onclick = function() {
     }
 }
 
+
+// Delete Logic
+document.getElementById('btnDelete').onclick = function(){
+
+
+    clickCount = -1;
+    
+    for(let i = 0; i < myArr.length; i++){
+        myArr[i] = delArray[i];
+    }
+
+
+
+    document.getElementById('quentity').innerHTML = "";
+
+
+    document.getElementById('labelName').innerHTML = "";
+
+    //Declare the Wheel elements;
+    document.getElementById('l1').innerHTML = myArr[0];
+    document.getElementById('l2').innerHTML = myArr[1];
+    document.getElementById('l3').innerHTML = myArr[2];
+    document.getElementById('l4').innerHTML = myArr[3];
+    document.getElementById('l5').innerHTML = myArr[4];
+    document.getElementById('l6').innerHTML = myArr[5];
+    document.getElementById('l7').innerHTML = myArr[6];
+    document.getElementById('l8').innerHTML = myArr[7];
+}
+
+
+
+
 // Spin Logic
 document.querySelector('.spinBtn').onclick = function(){
-    wheel.style.transform = "rotate(" + value + "deg)";
-    value += Math.ceil(Math.random() * 3600);
+    wheel.style.transform = "rotate(" + randWheel + "deg)";
+    randWheel += Math.ceil(Math.random() * 3600);
 
     
     var audio = document.getElementById('audio');
